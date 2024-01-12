c9705be6f9b3b7b083a1362fd1e2ad883197f510
KarlaCristinaS <karlacsilva.mg@gmail.com>
2024-01-11 23:21:00 -0300

    desafio  concluido

--- SRC/index.js
+++ SRC/index.js
@@ -1,2 +1,20 @@
-let nome = "Felipe";
-console.log(nome);
+let nomeHeroi = ""
+let QuantExperiencia = 902
+
+if(QuantExperiencia < 1000)
+        {nomeHeroi = "Ferro"}     
+else if(QuantExperiencia >= 1001 && QuantExperiencia <=2000)
+        {nomeHeroi = "Bronze"}
+else if(QuantExperiencia >= 2001 && QuantExperiencia <=5000)
+        {nomeHeroi = "Prata"}
+else if(QuantExperiencia >= 6001 && QuantExperiencia <=7000)
+        {nomeHeroi = "Ouro"}
+else if(QuantExperiencia >= 7001 && QuantExperiencia <=8000)
+        {nomeHeroi = "Platina"}
+else if(QuantExperiencia >= 8001 && QuantExperiencia <=9000)
+        {nomeHeroi = "Imortal"}        
+else
+        {nomeHeroi = "Radiante"}
+
+console.log("Nome do Heroi: " + nomeHeroi)
+console.log("Quantidade de experiência: " + QuantExperiencia)
